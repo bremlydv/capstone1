ec4b4a3a305685f8c8168f3478d8b9ef830d3d3b
bremlydv <bremlydv@gmail.com>
2018-09-25 20:15:52 +0800

    updated files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
     
     
-    /* For the sticky navigation */
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-    
-    
     /* Scroll on buttons */
-    $('#js--scroll-to-newArrivals').click(function () {
-       $('html, body').animate({scrollTop: $('#newArrivalSection').offset().top}, 1000); 
+    $('.js--scroll-to-newArrivals').click(function () {
+       $('html, body').animate({scrollTop: $('.js--newArrivalSection').offset().top}, 1000); 
     });
     
-    $('.js--scroll-to-start').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    $('.js--scroll-to-womenSection').click(function () {
+       $('html, body').animate({scrollTop: $('.js--womenSection').offset().top}, 1000); 
     });
     
+    $('.js--scroll-to-menSection').click(function () {
+       $('html, body').animate({scrollTop: $('.js--menSection').offset().top}, 1000); 
+    });
+
+    $('.js--scroll-to-sportsWearSection').click(function () {
+       $('html, body').animate({scrollTop: $('.js--sportswearSection').offset().top}, 1000); 
+    });
     
     /* Navigation scroll */
     $(function() {
@@ -66,19 +61,6 @@ $(document).ready(function() {
     });
     
     
-    /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        
-        nav.slideToggle(200);
-        
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }        
-    });
+
+
 });
